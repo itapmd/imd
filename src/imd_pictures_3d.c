3766488a9f49f1c98f876f3269ef16391fc78bb1
Franz Gähler <gaehler@math.uni-bielefeld.de>
1999-07-12 14:44:43 +0000

    bug in write_pic_cell fixed; vrml eliminated (better done offline)

--- src/imd_pictures_3d.c
+++ src/imd_pictures_3d.c
@@ -33,80 +33,15 @@ void write_pic_cell( cell *p, FILE *out )
         if (Epot_diff==1) {
           picbuf.E_pot = (float) p->pot_eng[i] - p->Epot_ref[i];
         } else
-#else
+#endif
         picbuf.E_pot = (float) p->pot_eng[i];
-#endif
         picbuf.type  = (integer) p->sorte[i];
-        fwrite( &picbuf, sizeof( picbuf ), 1, out ); 
+#else
+        picbuf.E_kin = (float) ( (px*px + py*py + pz*pz) / 2 );
+        picbuf.type  = (integer)1;
 #endif /* MONOLJ */
+        fwrite( &picbuf, sizeof( picbuf ), 1, out ); 
     }
-}
-
-
-void write_vrml_cell( cell *p, FILE *out ) 
-{
-
-    real px, py, pz, E_pot, E_kin;
-    real red, green, blue;
-    int i, ind;
-    real tabred[5],tabgreen[5],tabblue[5];
-
-/*  Original table from RUS - we use more saturation
-
-    tabred[0] = 0.10; tabgreen[0] = 0.20; tabblue[0] = 0.50;
-    tabred[1] = 0.05; tabgreen[1] = 0.75; tabblue[1] = 0.75;
-    tabred[2] = 0.10; tabgreen[2] = 0.50; tabblue[2] = 0.25;
-    tabred[3] = 0.75; tabgreen[3] = 0.75; tabblue[3] = 0.05;
-    tabred[4] = 0.75; tabgreen[4] = 0.05; tabblue[4] = 0.05; */
-
-    tabred[0] = 0.02; tabgreen[0] = 0.02; tabblue[0] = 0.45;
-    tabred[1] = 0.03; tabgreen[1] = 0.23; tabblue[1] = 0.23;
-    tabred[2] = 0.02; tabgreen[2] = 0.45; tabblue[2] = 0.02;
-    tabred[3] = 0.23; tabgreen[3] = 0.23; tabblue[3] = 0.03;
-    tabred[4] = 0.45; tabgreen[4] = 0.02; tabblue[4] = 0.02; 
-  
-    for (i = 0;i < p->n; ++i) {
-        px = p->impuls X(i);
-        py = p->impuls Y(i);
-        pz = p->impuls Z(i);
-#ifndef MONOLJ
-        E_kin = (float) ( (px*px + py*py + pz*pz) / (2 * p->masse[i]) );
-#else
-        E_kin = (float) ( (px*px + py*py + pz*pz) / 2 );
-#endif
-
-#ifndef MONOLJ
-#ifdef DISLOC
-	if (p->sorte[i] == dpotsorte) {
-    	  E_pot = (float) p->pot_eng[i] - p->Epot_ref[i];
-	  if (E_pot < min_dpot) continue;
-	}
-#else
-        E_pot = (float) (p->pot_eng[i]+35)/82;
-#endif
-#else
-	E_pot=0;
-#endif
-	ind = (int)(E_pot * 3.9999);
-	ind = (ind -120)/4;
-	if (ind < -1) continue;
-	if (ind > 3) continue;
-
-        /* Get RGB values from linear interpolation */
-	red = -4.0 * (tabred[ind] - tabred[ind+1]) * E_pot + (tabred[ind] * (ind+1) - ind * tabred[ind+1] );
-
-	green = -4.0 * (tabgreen[ind] - tabgreen[ind+1]) * E_pot + (tabgreen[ind] * (ind+1) - ind * tabgreen[ind+1] );
-
-        blue = -4.0 * (tabblue[ind] - tabblue[ind+1]) * E_pot + (tabblue[ind] * (ind+1) - ind * tabblue[ind+1] );
-
-        fprintf(out, "  Separator {\n");
-	fprintf(out, "    Material { diffuseColor %f %f %f }\n", red, green, blue);
-        fprintf(out, "    Translation { translation %f %f %f }\n", p->ort X(i), p->ort Y(i), p->ort Z(i));
-#ifndef MONOLJ
-	fprintf(out, "    Sphere { radius %f }\n", (float)((p->sorte[i] + 1)*.3));
-	fprintf(out, " }\n"); 
-#endif
-      }
 }
 
 
@@ -227,7 +162,7 @@ void write_pictures_raw(int steps)
 *
 ******************************************************************************/
 
-void write_pictures_bins(int steps)
+void write_pictures_atoms(int steps)
 
 #define XRES 720
 #define YRES 576
@@ -652,210 +587,21 @@ if (0==myid) {
 #endif
 }
 
-void write_vrmls(int steps)
-{ 
-  FILE *out;
-
-  str255 fname;
-  int fzhlr;
-  cell *p,*q;
-  int i,j,k,l,m,tag;
-  real phi;
-
-  /* Dateiname fuer Ausgabedatei erzeugen */
-  fzhlr = steps / pic_interval;
-  phi = acos(-view_dir.z / sqrt(view_dir.x*view_dir.x+view_dir.y*view_dir.y+view_dir.z*view_dir.z));
-  
-#ifdef MPI  
-  if (0<parallel_output)
-    sprintf(fname,"%s.%u.%u.wrl",outfilename,fzhlr,myid);
-  else
-#endif
-    sprintf(fname,"%s.%u.wrl",outfilename,fzhlr);
-
-
-#ifdef MPI
-
-  if (0<parallel_output) {
-
-    /* Ausgabedatei oeffnen */
-    out = fopen(fname,"w");
-    if (NULL == out) error("Cannot open output file for config.");
-    fprintf(out, "#VRML V1.0 ascii\n\n");
-    fprintf(out, "DEF StartPers PerspectiveCamera {\n");
-    fprintf(out, "position 0 0 0\n");
-    fprintf(out, "orientation 1 1 1 0\n");
-    fprintf(out, "focalDistance 10\n");
-    fprintf(out, "}\n\n");
-    fprintf(out, "Separator {\n");
-
-    for (i = 1; i < cell_dim.x-1; ++i )
-      for (j = 1; j < cell_dim.y-1; ++j )
-        for (k = 1; k < cell_dim.z-1; ++k ) {
- 	  p = PTR_3D_V(cell_array, i, j, k, cell_dim);
-	  write_vrml_cell( p, out );
-	};
-    
-    fprintf(out, "}\n");
-    fclose(out);
-
-  } else {
-
-    if (0==myid) {
-
-      /* Ausgabedatei oeffnen */
-      out = fopen(fname,"w");
-      if (NULL == out) error("Cannot open output file for config.");
-      fprintf(out, "#VRML V1.0 ascii\n\n");
-      fprintf(out, "DEF StartPers PerspectiveCamera {\n");
-      fprintf(out, "position 2.5 2.5 15\n");
-      fprintf(out, "orientation 0 0 1 0\n");
-      fprintf(out, "focalDistance 10\n");
-      fprintf(out, "}\n\n");
-      fprintf(out, "Separator {\n");
-
-
-      /* Write data on CPU 0 */
-
-      /* Write own data */
-    for (i = 1; i < cell_dim.x-1; ++i )
-      for (j = 1; j < cell_dim.y-1; ++j )
-        for (k = 1; k < cell_dim.z-1; ++k ) {
-	  p = PTR_3D_V(cell_array, i, j, k, cell_dim);
-	  write_vrml_cell( p, out );
-	};
-
-      /* Receive data from other cpus and write that */
-      p   = PTR_3D_V(cell_array, 0, 0, 0, cell_dim);
-      for ( m = 1; m < num_cpus; ++m)
-        for (i = 1; i < cell_dim.x-1; ++i )
-          for (j = 1; j < cell_dim.y-1; ++j )
-            for (k = 1; k < cell_dim.z-1; ++k ) {
-#ifndef MONOLJ
-	      tag = PTR_3D_V(CELL_TAG, i, j, k, cell_dim);
-	      recv_cell( p, m, tag );
-#else
-	      recv_cell( p, m, ORT_TAG );
-#endif
-	      write_vrml_cell( p, out );
-	    };
-
-      fprintf(out, "}\n");
-      fclose(out);      
-    } else { 
-      /* Send data to cpu 0 */
-        for (i = 1; i < cell_dim.x-1; ++i )
-          for (j = 1; j < cell_dim.y-1; ++j )
-            for (k = 1; k < cell_dim.z-1; ++k ) {
-      	      p   = PTR_3D_V(cell_array, i, j, k, cell_dim);
-#ifndef MONOLJ
-	      tag = PTR_3D_V(CELL_TAG, i, j, k, cell_dim);
-	      send_cell( p, 0, tag );
-#else
-	      send_cell( p, 0, ORT_TAG );
-#endif
-	    };
-    };
-  };
-#else
-
-  /* Ausgabedatei oeffnen */
-  out = fopen(fname,"w");
-  if (NULL == out) error("Cannot open output file for config.");
-  fprintf(out, "#VRML V1.0 ascii\n\n");
-  fprintf(out, "Separator {\n");
-  if (projection)
-    fprintf(out, "  PerspectiveCamera {\n");
-  else
-    fprintf(out, "  OrthographicCamera {\n");
-  fprintf(out, "  position %f %f %f\n", view_pos.x, view_pos.y, view_pos.z);
-  fprintf(out, "  orientation %f %f 0 %f\n", view_dir.y, -view_dir.x, phi);
-  fprintf(out, "  focalDistance 1\n");
-  fprintf(out, "  }\n");
-  fprintf(out, "  Separator {\n");
-  fprintf(out, "    Material { diffuseColor 1 1 1 transparency 0.7 }\n");
-  fprintf(out, "    Translation { translation %f %f %f }\n", box_x.x/2, box_y.y/2, box_z.z/2);
-  fprintf(out, "    Cube { width %f depth %f height %f }\n", box_x.x, box_y.y, box_z.z);
-  fprintf(out, "  }\n");
-  fprintf(out, "  Separator {\n");
-  fprintf(out, "    Material { diffuseColor 1 0 0 }\n");
-  fprintf(out, "    Translation { translation 0 0 %f }\n", box_z.z/2);
-  fprintf(out, "    Rotation { rotation 1 0 0 1.5707963 }\n");
-  fprintf(out, "    Cylinder { radius .1 height %f}\n", box_z.z*1.1);
-  fprintf(out, "  }\n");
-  fprintf(out, "  Separator {\n");
-  fprintf(out, "    Material { diffuseColor 0 1 0}\n");
-  fprintf(out, "    Translation { translation %f 0 0 }\n", box_x.x/2);
-  fprintf(out, "    Rotation { rotation 0 0 -1 1.5707963 }\n");
-  fprintf(out, "    Cylinder { radius .1 height %f}\n", box_x.x*1.1);
-  fprintf(out, "  }\n");
-  fprintf(out, "  Separator {\n");
-  fprintf(out, "    Material { diffuseColor 0 0 1 }\n");
-  fprintf(out, "    Translation { translation 0 %f 0 }\n", box_y.y/2);
-  fprintf(out, "    Cylinder { radius .1 height %f}\n", box_y.y*1.1);
-  fprintf(out, "  }\n");
-  fprintf(out, "  Separator {\n");
-  fprintf(out, "    Material { diffuseColor 1 0 0 }\n");
-  fprintf(out, "    Translation { translation 0 0 %f }\n", box_z.z*1.1);
-  fprintf(out, "    Rotation { rotation 1 0 0 1.5707963 }\n");
-  fprintf(out, "    Cone { radius .1 height 1}\n");
-  fprintf(out, "  }\n");
-  fprintf(out, "  Separator {\n");
-  fprintf(out, "    Material { diffuseColor 0 1 0}\n");
-  fprintf(out, "    Translation { translation %f 0 0 }\n", box_x.x*1.1);
-  fprintf(out, "    Rotation { rotation 0 0 -1 1.5707963 }\n");
-  fprintf(out, "    Cone { radius .1 height 1}\n");
-  fprintf(out, "  }\n");
-  fprintf(out, "  Separator {\n");
-  fprintf(out, "    Material { diffuseColor 0 0 1 }\n");
-  fprintf(out, "    Translation { translation 0 %f 0 }\n", box_y.y*1.1);
-  fprintf(out, "    Cone { radius .1 height 1}\n");
-  fprintf(out, "  }\n");
-  fprintf(out, "  Separator {\n");
-  fprintf(out, "    Material { diffuseColor 1 0 0 }\n");
-  fprintf(out, "    Translation { translation 0 0 %f }\n", box_z.z*1.1);
-  fprintf(out, "    Rotation { rotation 1 0 0 1.5707963 }\n");
-  fprintf(out, "    AsciiText { string \"z\"}\n");
-  fprintf(out, "  }\n");
-  fprintf(out, "  Separator {\n");
-  fprintf(out, "    Material { diffuseColor 0 1 0}\n");
-  fprintf(out, "    Translation { translation %f 0 0 }\n", box_x.x*1.1);
-  fprintf(out, "    Rotation { rotation 0 0 -1 1.5707963 }\n");
-  fprintf(out, "    AsciiText { string \"x\"}\n");
-  fprintf(out, "  }\n");
-  fprintf(out, "  Separator {\n");
-  fprintf(out, "    Material { diffuseColor 0 0 1 }\n");
-  fprintf(out, "    Translation { translation 0 %f 0 }\n", box_y.y*1.1);
-  fprintf(out, "    AsciiText { string \"y\"}\n");
-  fprintf(out, "  }\n");
-
-
-
-
-  for (p = cell_array; 
-       p <= PTR_3D_V(cell_array,
-		     cell_dim.x-1,
-		     cell_dim.y-1,
-		     cell_dim.z-1,
-		     cell_dim);
-       ++p ) {
-    write_vrml_cell( p , out );
-  };
-
-  fprintf(out, "}\n");
-  fclose(out);  
-
-#endif
-
-}
-
 
 void write_pictures( steps )
 {
   switch (pic_type) {
     case 0: write_pictures_raw ( steps ); break;
-    case 1: break;
-    case 2: write_pictures_bins( steps ); break;
-    case 3: write_vrmls( steps ); break;
+    case 1: write_pictures_atoms ( steps ); break;
+    case 2: break;
   };
 }
+
+
+
+
+
+
+
+
+
