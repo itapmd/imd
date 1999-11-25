8b8d0cd974549269789ad837e85edac276ff1ab9
Christoph Rudhart <rudhart@web.de>
1999-11-25 14:45:18 +0000

    tokens moved from makros.h to client.h

--- 2dvis/makros.h
+++ 2dvis/makros.h
@@ -2,6 +2,4 @@
 #define ABS(a) ((a) >0 ? (a) : -(a))
 
 #define COLRES 245
-#define T_CONF 9
-#define T_DIST 1
 #define DIM 2
--- 2dvis/tokens.h
+++ 2dvis/tokens.h
@@ -1,7 +1,7 @@
 #ifndef _TOKENS_H_
 #define _TOKENS_H_
 
-#define T_GET           9
+#define T_CONF           9
 #define T_QUIT          99
 #define T_DISTRIBUTION  1
 #define T_PICTURE  2
