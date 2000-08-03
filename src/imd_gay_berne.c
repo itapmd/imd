3ea830201375282c5951091494dff6e54cf1a5b3
Franz Gähler <gaehler@math.uni-bielefeld.de>
2000-08-03 16:47:23 +0000

    accelerated uniax by a factor 5 - basically by eliminating the pow function.

--- src/imd_gay_berne.c
+++ src/imd_gay_berne.c
@@ -1,4 +1,3 @@
-#ifdef UNIAX
 
 /********************************************************/
 /*   interaction between two identical                  */
@@ -21,6 +20,11 @@ void gay_berne ( vektor r12, vektor e1, vektor e2, real rsqr,
   const real mu = 2.0 ;
   const real nu = 1.0 ;
   const real omu = 1.0 / mu ;
+  /* the following depend on the value of mu, nu, and omu */
+  /*   avoid pow if possible - it's terribly slow */
+#define POWMU(x) (x)*(x)
+#define POWNU(x) (x)
+#define POWOMU(x) sqrt(x)
   
   /* uniaxial Gay-Berne parameters */
 
@@ -44,10 +48,13 @@ void gay_berne ( vektor r12, vektor e1, vektor e2, real rsqr,
   real aux, aux2, aux6, aux12 ;
   real rep, rem, repp, remp ;
   real pot_rr, pot_re1, pot_re2, pot_e12 ;
+  real den, den2, rep2, rem2, denp, denm, denpp, denmp, tmp;
+  real tmpx, tmpz;
 
   /******************************************************/
 
   /* check for uniaxiality */
+  /* this should not be done in the innermost loop - every time!!!! */
 
   if ( s1.x != s1.y ) {
     error("not a uniaxial molecule!\n"); }
@@ -57,11 +64,11 @@ void gay_berne ( vektor r12, vektor e1, vektor e2, real rsqr,
 
   /* Gay-Berne anisotropy parameters */
 
-  chi = ( pow(s1.z,2.0)  - pow(s1.x,2.0) )
-    / ( pow(s1.z,2.0) + pow(s1.x,2.0) ) ;
+  chi = ( SQR(s1.z) - SQR(s1.x) ) / ( SQR(s1.z) + SQR(s1.x) ) ;
 
-  chip = ( pow(w1.x,omu) - pow(w1.z,omu) )
-    / ( pow(w1.x,omu) + pow(w1.z,omu) ) ;
+  tmpx = POWOMU( w1.x );
+  tmpz = POWOMU( w1.z );
+  chip = ( tmpx - tmpz ) / ( tmpx + tmpz ) ;
 
   /* minimum contact distance */
 
@@ -105,29 +112,35 @@ void gay_berne ( vektor r12, vektor e1, vektor e2, real rsqr,
   re2 = orr * SPROD(r12,e2) ;
   e12 = SPROD(e1,e2) ;
 
+  /* some common subexpressions */
+
+  rep2  = re1 + re2 ;  rep2 = rep2 * rep2 ;
+  rem2  = re1 - re2 ;  rem2 = rem2 * rem2 ;
+  denp  = 1.0 + chi  * e12 + tiny ;
+  denm  = 1.0 - chi  * e12 + tiny ;
+  denpp = 1.0 + chip * e12 + tiny ;
+  denmp = 1.0 - chip * e12 + tiny ;
+  den2  = 1.0 - chi  * chi * e12 * e12 + tiny ;
+
   /******************************************************/
 
   /* Gay-Berne epsilon function */
 
-  eps = eps0 / sqrt( 1.0 - pow( chi * e12, 2.0 ) + tiny ) ;
+  eps = eps0 / sqrt( den2 ) ;
 
   /* Gay-Berne epsilon prime function */
 
-  epsp = 1.0 - 0.5 * chip * 
-    ( pow( re1 + re2, 2.0 ) / ( 1.0 + chip * e12 + tiny ) +
-      pow( re1 - re2, 2.0 ) / ( 1.0 - chip * e12 + tiny ) ) ;
-      
+  epsp = 1.0 - 0.5 * chip * ( rep2 / denpp + rem2 / denmp ) ;
+
   /* Gay-Berne well */
 
-  well = pow( eps, nu ) * pow( epsp, mu ) ;
+  well = POWNU(eps) * POWMU(epsp) ;
 
   /* Gay-Berne sigma function */
 
-  aid = 1.0 - 0.5 * chi * 
-    ( pow( re1 + re2, 2.0 ) / ( 1.0 + chi * e12 + tiny ) +
-      pow( re1 - re2, 2.0 ) / ( 1.0 - chi * e12 + tiny ) ) ;
-  
-  sig = sig0 / sqrt( aid + tiny ) ;
+  aid = 1.0 - 0.5 * chi * ( rep2 / denp + rem2 / denm ) ;
+  den = sqrt( aid + tiny ) ;
+  sig = sig0 / den ;
 
   /* Gay-Berne 12-6 expressions */
 
@@ -147,102 +160,92 @@ void gay_berne ( vektor r12, vektor e1, vektor e2, real rsqr,
       /***************************************************/
       /* forces and torques between pair of molecules */
 
-      rep = ( re1 + re2 ) / ( 1.0 + chi * e12 + tiny ) ;
-      rem = ( re1 - re2 ) / ( 1.0 - chi * e12 + tiny ) ;
+      rep = ( re1 + re2 ) / denp ;
+      rem = ( re1 - re2 ) / denm ;
 
-      repp = ( re1 + re2 ) / ( 1.0 + chip * e12 + tiny ) ;
-      remp = ( re1 - re2 ) / ( 1.0 - chip * e12 + tiny ) ;
+      repp = ( re1 + re2 ) / denpp ;
+      remp = ( re1 - re2 ) / denmp ;
 
       /* derivative of potential energy 
 	 with respect to molecular distance r(12) */
       
-      pot_rr = well * ( - 12.0 * aux12 + 6.0 * aux6 )
-	* aux / sig0 ;
+      tmp    = 3.0 * well * aux * ( 2.0 * aux12 - aux6 ) ;
+      pot_rr = - 2.0 * tmp / sig0 ;
+      tmp    = tmp * chi / ( den * den * den );
 
       /* derivative of potential energy 
 	 with respect to cos(theta1) */
 
       pot_re1 = - well * mu * chip * ( repp + remp )
 	* ( aux12 - aux6 ) / ( epsp + tiny )
-	+ 3.0 * well * chi * ( rep + rem ) * aux
-	* ( 2.0 * aux12 - aux6 ) 
-	/ sqrt( pow( aid + tiny, 3.0 ) ) ;
+	+ tmp * ( rep + rem ) ;
 
       /* derivative of potential energy 
 	 with respect to cos(theta2) */
-      
+
       pot_re2 = - well * mu * chip * ( repp - remp )
 	* ( aux12 - aux6 ) / ( epsp + tiny )
-	+ 3.0 * well * chi * ( rep - rem ) * aux
-	* ( 2.0 * aux12 - aux6 ) 
-	/ sqrt( pow( aid + tiny, 3.0 ) ) ;
+	+ tmp * ( rep - rem ) ;
 
       /* derivative of potential energy 
 	 with respect to cos(phi12) */
 
-      pot_e12 = well * nu * pow( chi, 2.0 ) * e12
-	* ( aux12 - aux6 ) 
-	/ ( 1.0 - pow( chi * e12, 2.0 ) + tiny )
-	- 0.5 * well * mu * pow( chip, 2.0 )
-	* ( aux12 - aux6 ) 
-	* ( - pow( re1 + re2, 2.0 ) /
-	    pow( 1.0 + chip * e12 + tiny, 2.0 )
-	    + pow( re1 - re2, 2.0 ) /
-	    pow( 1.0 - chip * e12 + tiny, 2.0 ) )
-	/ ( epsp + tiny )
-	+ 0.5 * well * pow( chi, 2.0 ) * aux
-	* ( 6.0 * aux12 - 3.0 * aux6 ) 
-	* ( - pow( re1 + re2, 2.0 ) /
-	    pow( 1.0 + chi * e12 + tiny, 2.0 )
-	    + pow( re1 - re2, 2.0 ) /
-	    pow( 1.0 - chi * e12 + tiny, 2.0 ) )
-	/ sqrt( pow( aid + tiny , 3.0 ) ) ;
+      pot_e12 = well * ( aux12 - aux6 ) * 
+        ( nu * chi * chi * e12 / den2 - 0.5 * mu * chip * chip *
+          ( remp * remp - repp * repp ) / ( epsp + tiny ) )
+        + 0.5 * tmp * chi * ( rem * rem - rep * rep ) ;
+
+      /* rescale */
+      pot_rr  *= orr ;
+      pot_re1 *= orr ;
+      pot_re2 *= orr ;
+      re1     *= orr ;
+      re2     *= orr ;
 
       /* force on molecule 1 due to molecule 2 */
 
       force12->x = - 4.0 * 
-	( orr * pot_rr * r12.x + pot_re1 * orr *
-	  ( e1.x - orr * re1 * r12.x ) + pot_re2 * orr *
-	  ( e2.x - orr * re2 * r12.x ) ) ;
+	( pot_rr * r12.x + 
+          pot_re1 * ( e1.x - re1 * r12.x ) + 
+          pot_re2 * ( e2.x - re2 * r12.x ) ) ;
 
-      force12->y = - 4.0 * 
-	( orr * pot_rr * r12.y + pot_re1 * orr *
-	  ( e1.y - orr * re1 * r12.y ) + pot_re2 * orr *
-	  ( e2.y - orr * re2 * r12.y ) ) ;
+      force12->y = - 4.0 *
+	( pot_rr * r12.y + 
+          pot_re1 * ( e1.y - re1 * r12.y ) + 
+          pot_re2 * ( e2.y - re2 * r12.y ) ) ;
 
-      force12->z = - 4.0 * 
-	( orr * pot_rr * r12.z + pot_re1 * orr *
-	  ( e1.z - orr * re1 * r12.z ) + pot_re2 * orr *
-	  ( e2.z - orr * re2 * r12.z ) ) ;
+      force12->z = - 4.0 *
+	( pot_rr * r12.z + 
+          pot_re1 * ( e1.z - re1 * r12.z ) + 
+          pot_re2 * ( e2.z - re2 * r12.z ) ) ;
       
       /* torque on molecule 1 due to molecule 2 */
       
       torque12->x = - 4.0 *
-	( orr * pot_re1 * ( e1.y * r12.z - e1.z * r12.y )
-	  + pot_e12 * ( e1.y * e2.z - e1.z * e2.y ) ) ;
+	( pot_re1 * ( e1.y * r12.z - e1.z * r12.y ) +
+	  pot_e12 * ( e1.y * e2.z - e1.z * e2.y ) ) ;
 
       torque12->y = - 4.0 *
-	( orr * pot_re1 * ( e1.z * r12.x - e1.x * r12.z )
-	  + pot_e12 * ( e1.z * e2.x - e1.x * e2.z ) ) ;
+	( pot_re1 * ( e1.z * r12.x - e1.x * r12.z ) +
+	  pot_e12 * ( e1.z * e2.x - e1.x * e2.z ) ) ;
 
       torque12->z = - 4.0 *
-	( orr * pot_re1 * ( e1.x * r12.y - e1.y * r12.x )
-	  + pot_e12 * ( e1.x * e2.y - e1.y * e2.x ) ) ;
+	( pot_re1 * ( e1.x * r12.y - e1.y * r12.x ) +
+	  pot_e12 * ( e1.x * e2.y - e1.y * e2.x ) ) ;
 
       /* torque on molecule 2 due to molecule 1 */
       
-      torque21->x = - 4.0 * 
-	( orr * pot_re2 * ( e2.y * r12.z - e2.z * r12.y )
-	  + pot_e12 * ( e2.y * e1.z - e2.z * e1.y ) ) ;
+      torque21->x = - 4.0 *
+	( pot_re2 * ( e2.y * r12.z - e2.z * r12.y ) +
+	  pot_e12 * ( e2.y * e1.z - e2.z * e1.y ) ) ;
 
-      torque21->y = - 4.0 * 
-	( orr * pot_re2 * ( e2.z * r12.x - e2.x * r12.z )
-	  + pot_e12 * ( e2.z * e1.x - e2.x * e1.z ) ) ;
+      torque21->y = - 4.0 *
+	( pot_re2 * ( e2.z * r12.x - e2.x * r12.z ) +
+	  pot_e12 * ( e2.z * e1.x - e2.x * e1.z ) ) ;
 
-      torque21->z = - 4.0 * 
-	( orr * pot_re2 * ( e2.x * r12.y - e2.y * r12.x )
-	  + pot_e12 * ( e2.x * e1.y - e2.y * e1.x ) ) ;
+      torque21->z = - 4.0 *
+	( pot_re2 * ( e2.x * r12.y - e2.y * r12.x ) +
+	  pot_e12 * ( e2.x * e1.y - e2.y * e1.x ) ) ;
     }
 }
 
-#endif
