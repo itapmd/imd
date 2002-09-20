62470768c12de602db1439f8bc88c272fef4b95c
Gunther Schaaf <gunther.schaaf@de.bosch.com>
2002-09-20 20:48:32 +0000

    Dynamic array and normalization flag added

--- util/sample2dist.c
+++ util/sample2dist.c
@@ -1,47 +1,83 @@
+/******************************************************************************
+*
+*  sample2dist is a utility program to convert checkpoint files to
+*  atoms distribution files.
+*
+*  Compilation:
+*
+*    gcc -o sample2dist sample2dist.c -lm
+*
+*  Usage:
+*
+*     sample2dist <dimx> <dimy> <dimz> <chkpt-file> [--normalize]
+*
+*  dimx, etc. are the distribution sizes in the three directions
+*  if --normalize is used the bins are normalize by the number of atoms
+*  they contain
+*
+*  Two files <chkpt-file>.pot.dist and ...kin.dist are created
+*
+******************************************************************************/              
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
 
 int main(int argc, char **argv) {
-  FILE *fp;
-  char line[255], str[100];
-  float m, x, y, z, vx, vy, vz, e, corr, boxx, boxy, boxz;
-  int n, t, i, j, k, sx, sy, sz;
-  int anz[50][50][12];
-  float smp[50][50][12];
+  FILE *fp, *pot, *kin;
+  char line[255], str[255], potstr[255], kinstr[255];
+  float m, x, y, z, vx, vy, vz, e, boxx, boxy, boxz;
+  int n, t, i, j, k, pos, sx, sy, sz, size, norm = 0;
+  int *anz;
+  float *smpp, *smpk;
 
-  if (argc!=5) {
-    fprintf(stderr, "Usage: ./sampledist dimx dimy dimz chkpt-file!");
+  /* Usage */
+  if ((argc!=5)&&(argc!=6)) {
+    fprintf(stderr, "Usage: ./sampledist dimx dimy dimz chkpt-file [--normalize]!");
     exit(-1);
   }
 
+  /* Dimension of dist */
   sscanf(argv[1],"%d",&sx);
   sscanf(argv[2],"%d",&sy);
   sscanf(argv[3],"%d",&sz);
+  size = sx*sy*sz;
 
-  for (i=0;i<sx;i++)
-    for (j=0;j<sy;j++)
-      for (k=0;k<sz;k++) {
-	anz[i][j][k] = 0;
-	smp[i][j][k] = 0.0;
-      }
+  /* allocation */
+  anz = (int *)calloc(sx*sy*sz, sizeof(int));
+  smpp = (float *)calloc(sx*sy*sz, sizeof(float));
+  smpk = (float *)calloc(sx*sy*sz, sizeof(float));
 
+  /* normalize */
+  if ((argc==6)&&(!strcmp(argv[5], "--normalize")))
+      norm = 1;
+
+  /* init */
+  for (i=0;i<sx*sy*sz;i++) {
+      anz[i] = 0;
+      smpp[i] = 0.0;
+      smpk[i] = 0.0;
+  }
+
+  /* read chkpt file */
   fp=fopen(argv[4], "r");
   /* header */
   fgets(line, 255, fp);
   fgets(line, 255, fp);
   fgets(line, 255, fp);
-  sscanf(line, "%s %s %f %f %f", str, str, &boxx, &boxy, &boxz);
+  sscanf(line, "%s %f %f %f", str, &boxx, &boxy, &boxz);
   fgets(line, 255, fp);
-  sscanf(line, "%s %s %f %f %f", str, str, &corr, &boxy, &boxz);
+  sscanf(line, "%s %f %f %f", str, &boxy, &boxy, &boxz);
   fgets(line, 255, fp);
-  sscanf(line, "%s %s %f %f %f", str, str, &boxz, &boxz, &boxz);
+  sscanf(line, "%s %f %f %f", str, &boxz, &boxz, &boxz);
   fgets(line, 255, fp);
   fgets(line, 255, fp);
 
+  /* atom lines */
   while(fgets(line, 255, fp)) {
     sscanf(line, "%d %d %f %f %f %f %f %f %f %f\n",
 	   &n, &t, &m, &x, &y, &z, &vx, &vy, &vz, &e);
-    x -= corr/boxx*y;
+
+    /* Coordinates of bin */
     i = (int)floor(sx*x/boxx);
     j = (int)floor(sy*y/boxy);
     k = (int)floor(sz*z/boxz);
@@ -51,18 +87,44 @@ int main(int argc, char **argv) {
     i = (i<sx)?i:sx-1;
     j = (j<sy)?j:sy-1;
     k = (k<sz)?k:sz-1;
-    smp[i][j][k] += vx*vx+vy*vy+vz*vz;
-    anz[i][j][k]++;
+    pos = i*sy*sz + j*sz + k;
+
+    /* values into bin */
+    smpp[pos] += e;
+    smpk[pos] += vx*vx+vy*vy+vz*vz;
+    anz[pos]++;
   }
   fclose(fp);
   
-  for (i=0;i<sx;i++)
-    for (j=0;j<sy;j++)
-      for (k=0;k<sz;k++)
-	if (anz[i][j][k]>0)
-	  printf("%d %d %d %f\n", i, j, k, smp[i][j][k]/anz[i][j][k]);
-	else
-	  printf("%d %d %d 0\n", i, j, k);
+  /* output, 1st header */
+  sprintf(potstr, "%s.pot.dist", argv[4]);
+  sprintf(kinstr, "%s.kin.dist", argv[4]);
+  pot = fopen(potstr, "w");
+  kin = fopen(kinstr, "w");
+
+  fprintf(pot, "#F A 3 0 1\n");
+  fprintf(pot, "#C Epot\n");
+  fprintf(pot, "#D %d %d %d\n", sx, sy, sz);
+  fprintf(pot, "#S %f %f %f\n", boxx/(float)sx, boxy/(float)sy, boxz/(float)sz);
+  fprintf(pot, "#E\n");
+  fprintf(kin, "#F A 3 0 1\n");
+  fprintf(kin, "#C Ekin\n");
+  fprintf(kin, "#D %d %d %d\n", sx, sy, sz);
+  fprintf(kin, "#S %f %f %f\n", boxx/(float)sx, boxy/(float)sy, boxz/(float)sz);
+  fprintf(kin, "#E\n");
+
+  /* dist */
+  for (i=0;i<sx*sy*sz;i++)
+      if (norm) {
+	  fprintf(pot, "%f\n", smpk[i]/anz[i]);
+	  fprintf(kin, "%f\n", smpp[i]/anz[i]);
+      } else {
+	  fprintf(pot, "%f\n", smpp[i]);
+	  fprintf(kin, "%f\n", smpk[i]);
+      }
+
+  fclose(pot);
+  fclose(kin);
 }
 
 
