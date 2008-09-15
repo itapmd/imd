ae4f910c303edc2083a6bc2b672a7c4ac15bfa56
Peter Brommer <p.brommer@warwick.ac.uk>
2008-09-15 09:14:50 +0000

    Copyright year updated

--- src/imd_potential.c
+++ src/imd_potential.c
@@ -3,7 +3,7 @@
 *
 * IMD -- The ITAP Molecular Dynamics Program
 *
-* Copyright 1996-2007 Institute for Theoretical and Applied Physics,
+* Copyright 1996-2008 Institute for Theoretical and Applied Physics,
 * University of Stuttgart, D-70550 Stuttgart
 *
 ******************************************************************************/
